2fd2f419acd35c703c053037c724494e39e52503
Amirparsa-n <amirparsanaghibi@gmail.com>
2025-05-06 17:10:49 +0330

    Implement deleteComment functionality: add method to delete a comment

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -7,6 +7,7 @@ import commentModel from '@models/comment.model';
 import productModel from '@models/product.model';
 
 import { BaseController } from './base.controller';
+import { isValidObjectId } from 'mongoose';
 
 class CommentController extends BaseController {
     getComments = async (req: Request, res: Response): Promise<any> => {};
@@ -35,7 +36,20 @@ class CommentController extends BaseController {
     };
     updateComment = async (req: Request, res: Response): Promise<any> => {};
 
-    deleteComment = async (req: Request, res: Response): Promise<any> => {};
+    deleteComment = async (req: Request, res: Response): Promise<any> => {
+        const { id } = req.params;
+
+        if (!isValidObjectId(id)) {
+            return this.errorResponse(res, 'Invalid comment id');
+        }
+
+        const deletedComment = await commentModel.findByIdAndDelete(id);
+        if (!deletedComment) {
+            return this.errorResponse(res, 'Comment not found', 404);
+        }
+
+        return this.successResponse(res, deletedComment, 'Comment deleted successfully');
+    };
 
     // Reply Comment
     createReplyComment = async (req: Request, res: Response): Promise<any> => {};
