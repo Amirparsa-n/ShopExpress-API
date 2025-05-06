026b6eb75c47fb2e0cbdc8024aaae38a7fd988b4
Amirparsa-n <amirparsanaghibi@gmail.com>
2025-05-06 18:58:58 +0330

    Enhance roleGuard middleware: support multiple roles for access control

--- src/middlewares/roleGuard.middleware.ts
+++ src/middlewares/roleGuard.middleware.ts
@@ -1,11 +1,20 @@
 import type { NextFunction, Request, Response } from 'express';
 import type { Role } from 'src/types/user.types';
 
-export const roleGuard = (role: Role) => {
+export const roleGuard = (role: Role | Role[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        if (req.user.role !== role) {
+        let hasAccessRole = false;
+
+        if (Array.isArray(role)) {
+            hasAccessRole = role.some((role) => req.user.role === role);
+        } else {
+            hasAccessRole = req.user.role === role;
+        }
+
+        if (!hasAccessRole) {
             return res.status(403).json({ message: 'Forbidden' });
         }
+
         next();
     };
 };
